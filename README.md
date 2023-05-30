96491306a1f8f20d6baac0b06c2b613a96f232cf
José Felipe Paula de Sousa <felipe@pop-os.localdomain>
2023-05-30 12:06:13 -0300

    inital push

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto Jarvis
+
+Esse projeto é um script que utiliza a api da openAI para gerar as respostas
---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+import json
+
+
+API_KEY =  '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'
+
+HEADERS = {
+    'Authorization': f'Bearer {API_KEY}',
+    'Content-Type': 'application/json'
+}
+
+link_models = "https://api.openai.com/v1/models"
+
+req = requests
+
+# Pegando todos os medelos disponiveis pelo openAI
+model_list = req.get(link_models, headers=HEADERS)
+
+# Modelo disponivel selecionado
+model_id = 'gpt-3.5-turbo'
+
+# api de post do modelo
+links_post = 'https://api.openai.com/v1/chat/completions'
+
+print("Olá sou jarvis") 
+
+# Loop infinito para execução do Programa
+while True:
+    
+    user_input = str(input("User: "))
+
+    # conteudo do requisição post
+    body_message = {
+        "model": model_id,
+        "messages": [{"role":"user", "content": user_input}]
+    }
+
+    print(".......")
+    print("Calculando")
+    print('........')
+
+    # modelo da requisição para json
+    data = json.dumps(body_message)
+
+    # Resposta da requisição em formato json
+    res = req.post(links_post, headers=HEADERS, data=data)
+
+    # Resposta da requisição converttido para dict
+    response = res.json()
+    ai_response = response["choices"][0]["message"]["content"]
+
+    print("AI: " + ai_response)
+
